1ef67a93c0a9f1a20267c3fe140d9141b43c84b9
manudev-p <manuelpirez39@gmail.com>
2024-11-19 14:34:20 -0800

    fixed typo

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,6 +1,6 @@
 import Button from '@/components/Button'
 
-const CONTACT_TITLE = 'What&apos;s Next?'
+const CONTACT_TITLE = 'What’s Next?'
 const CONTACT_SUBTITLE = 'Get In Touch'
 const CONTACT_TEXT =
   'Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!'
